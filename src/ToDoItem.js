95a85b16ae353c9e04aadcc87758110309fd365d
Lukman Nakib <lukman.nakib>
2019-04-03 20:15:27 +0600

    complete

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -3,6 +3,14 @@ import React from "react"
 
 
 function ToDoItem(props) {
+
+	 const completedStyle = {
+        fontStyle: "italic",
+        color: "#cdcdcd",
+        textDecoration: "line-through",
+        transition: "all 0.3s ease-in-out 0s"
+    }
+
   return (
     <div className="todo-item">
 
@@ -11,7 +19,8 @@ function ToDoItem(props) {
      		checked={props.item.completed}
      		onChange={()=> {props.handleChnage(props.item.id,props.item.text)}}
      	/>
-            <p> {props.item.text}</p>
+           
+            <p style={props.item.completed ? completedStyle: null}>{props.item.text}</p>
     </div>
   )
 }
